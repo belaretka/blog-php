6c263a826fbb310c8863f9f638c1e4871d1b48c5
belaretka <ttnblchk@gmail.com>
2024-02-14 22:14:37 +0300

    add post page with parts for cru

---
+++ www/views/posts/parts/add.php
@@ -0,0 +1,23 @@
+<div class="card-body">
+    <form >
+        <div class="form-group">
+            <h3>Post title</h3>
+            <input type="text" class="form-control" placeholder="Post Title">
+        </div>
+        <div class="form-group">
+            <h3>Categories</h3>
+            <select multiple class="form-control" id="categories">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <h3>Post Content</h3>
+            <textarea class="form-control" id="content" rows="5" placeholder="Add content"></textarea>
+        </div>
+        <a href="<?= 'http://'.$_SERVER["SERVER_NAME"].'/?resource=posts&action=save'?>" type="submit" class="btn btn-primary btn-block">Submit</a>
+    </form>
+</div>
